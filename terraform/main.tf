c64a01627ab2a73cc7f5e2a53c91658bc85d8a32
tiwariParth <243mjparth@gmail.com>
2025-01-20 22:31:55 +0530

    Update AWS AMI filter to allow for flexible version matching

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
-  
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
